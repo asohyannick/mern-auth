fbc6b3f65260a7e63169fff2b74d992b54f60cb0
asohyannick <asohyannick3@gmail.com>
2024-02-18 09:19:00 -0800

    Install the neccessary template engines for the API and frontend tools for the UI

---
+++ api/index.js
@@ -0,0 +1,22 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 8000;
+// security packages
+app.use(cors());
+// middleware packages
+app.use(express.json());
+// routes
+app.get('/test',(req, res) => {
+    res.send('<h1>API is working successfully<h1>')
+});
+// db config
+mongoose.connect(process.env.MONGO_URI).then(() => {
+    console.log('MongoDB is connected successfully!')
+}).catch((error) => {console.log(error.message)})
+app.listen(PORT, function(req, res) {
+    console.log(`Server is running on port ${PORT}...`);
+});
---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <div className="bg-emerald-500">hello</div>
+    </>
+  )
+}
+
+export default App
