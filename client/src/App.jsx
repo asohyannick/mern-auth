82cb90aa31b18fea84eba822a7cc68a4b46eef8d
asohyannick <asohyannick3@gmail.com>
2024-02-24 07:33:01 -0800

    added routes and created pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,15 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { Home, About, SignIn, SignUp, Profile } from "./pages/index";
 function App() {
-  return (
-    <>
-      <div className="bg-emerald-500">hello</div>
-    </>
-  )
+  return <BrowserRouter>
+     <Routes>
+       <Route path="/" element={<Home/>}/>
+       <Route path="/about" element={<About/>}/>
+       <Route path="sign-in" element={<SignIn/>}/>
+       <Route path="sign-up" element={<SignUp/>}/>
+       <Route path="/profile" element={<Profile/>}/>
+       <Route path="*" element={<Navigate to='/'/>}/>
+     </Routes>
+  </BrowserRouter>;
 }
-
-export default App
+export default App;
---
+++ client/src/pages/index.jsx
@@ -0,0 +1,12 @@
+import Home from './Home';
+import About from './About';
+import SignIn from './SignIn';
+import SignUp from './SignUp';
+import Profile from './Profile';
+export {
+    Home,
+    About,
+    SignIn,
+    SignUp,
+    Profile
+}
