c3c620e7d0376f0ecaeabef9f562aeb50f8d6c0b
asohyannick <asohyannick3@gmail.com>
2024-02-27 01:13:58 -0800

    create user update API route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,3 +1,32 @@
-export const test = async(req, res, next) => {
-
-}
+import { StatusCodes } from "http-status-codes";
+import { errorHandler } from "../utils/error.js";
+import bcyptjs from "bcryptjs";
+import User from "../models/user.model.js";
+export const updateUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id) {
+    return next(
+      errorHandler(StatusCodes.UNAUTHORIZED, "You can update only your account")
+    );
+  }
+  try {
+    if (req.body.password) {
+      req.body.password = bcyptjs.hashSync(req.body.password, 10);
+    }
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password,
+          profilePicture: req.body.profilePicture,
+        },
+      },
+      { new: true }
+    );
+    const { password, ...rest } = updatedUser._doc;
+    res.status(StatusCodes.OK).json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
---
+++ api/utils/verifyUser.js
@@ -0,0 +1,14 @@
+import { StatusCodes } from "http-status-codes";
+import {errorHandler} from '../utils/error.js';
+import jwt from "jsonwebtoken";
+export const verifyToken = (req, res, next) => {
+  const token = req.cookies.access_token;
+  if (!token)
+    return next(errorHandler(StatusCodes.UNAUTHORIZED, 'Access denied because you have not created an account before trying to sign in'))
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err)
+      return next(errorHandler(errorHandler(StatusCodes.UNAUTHORIZED, 'Token is valid')));
+    req.user = user;
+    next();
+  });
+};
